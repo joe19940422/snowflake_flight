25339e54fbb94aba908ac5a3a197ff2734b30ea2
fei <pengfei.qiao@nielsen.com>
2025-03-06 21:56:37 +0100

    materialized='incremental'

--- models/mart/arrival_country_ranking.sql
+++ models/mart/arrival_country_ranking.sql
@@ -1,9 +1,25 @@
-{{ config(materialized='table') }}
 
-select
-ARRIVAL_COUNTRY_NAME,
-FLIGHT_NUMBER,
-FLIGHT_DATE,
-count(*) as cn
-from {{ ref('dwh_flights') }}
-group by ARRIVAL_COUNTRY_NAME, FLIGHT_DATE, FLIGHT_NUMBER order by cn desc
+{{ config(
+    materialized='incremental',
+    unique_key=['ARRIVAL_COUNTRY_NAME', 'FLIGHT_DATE', 'FLIGHT_NUMBER'],  # Add FLIGHT_NUMBER
+    incremental_strategy='upsert'
+) }}
+
+SELECT
+    ARRIVAL_COUNTRY_NAME,
+    FLIGHT_NUMBER,
+    FLIGHT_DATE,
+    COUNT(*) AS cn
+FROM
+    {{ ref('dwh_flights') }}
+
+{% if is_incremental() %}
+WHERE last_updated > (SELECT MAX(last_updated) FROM {{ this }})
+{% endif %}
+
+GROUP BY
+    ARRIVAL_COUNTRY_NAME,
+    FLIGHT_DATE,
+    FLIGHT_NUMBER  # Include in GROUP BY clause
+ORDER BY
+    cn DESC
