d8c54049027f76de7220969eb464858913e91232
fei <pengfei.qiao@nielsen.com>
2025-03-06 20:49:21 +0100

    convert to table

--- models/mart/arrival_country_ranking.sql
+++ models/mart/arrival_country_ranking.sql
@@ -1,6 +1,8 @@
-select 
-ARRIVAL_COUNTRY_NAME, 
-FLIGHT_DATE, 
-count(*) as cn 
-from flight.flight_dwh.dwh_flights 
-group by ARRIVAL_COUNTRY_NAME, FLIGHT_DATE order by cn desc 
+{{ config(materialized='table') }}
+
+select
+ARRIVAL_COUNTRY_NAME,
+FLIGHT_DATE,
+count(*) as cn
+from {{ ref('dwh_flights') }}
+group by ARRIVAL_COUNTRY_NAME, FLIGHT_DATE order by cn desc
--- models/mart/departure_country_ranking.sql
+++ models/mart/departure_country_ranking.sql
@@ -1,8 +1,8 @@
-
+{{ config(materialized='table') }}
 
 select 
 departure_country_name, 
 FLIGHT_DATE, 
 count(*) as cn 
-from flight.flight_dwh.dwh_flights 
+from {{ ref('dwh_flights') }}
 group by departure_country_name, FLIGHT_DATE order by cn desc 
